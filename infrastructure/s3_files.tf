9d71b80f69cf07ac33f01d46d9d2d9fd00c34f97
cleyber-cabral <30348791+cleyber-cabral@users.noreply.github.com>
2021-08-16 22:21:54 -0300

    Update s3_files.tf

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -9,7 +9,7 @@
 
 
 resource "aws_s3_bucket_object" "job_spark" {
-  bucket = aws_s3_bucket.dl
+  bucket = aws_s3_bucket.dl.id
   key    = "emr-code/pyspark/job_spark_from_tf.py" 
   acl    = "private"
   source = "../job_spark.py"
